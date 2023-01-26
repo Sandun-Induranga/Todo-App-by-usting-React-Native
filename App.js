ac6642fb66c581930206e212af1779926a884f72
Sandun Induranga <sandutharu40@gmail.com>
2023-01-26 22:09:18 +0530

    Content Updated

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-
       <View style={styles.tasks}>
-
         <Text style={styles.title}>Your Tasks</Text>
 
+        <View style={styles.items}></View>
       </View>
-      
     </View>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });

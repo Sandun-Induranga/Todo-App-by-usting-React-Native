880ce9afb38a09835e77d1f58f014b49f57d0032
Sandun Induranga <sandutharu40@gmail.com>
2023-01-26 22:50:15 +0530

    Content Updated

--- App.js
+++ App.js
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
   },
   items: {
     backgroundColor: "white",
-    height: 100,
+    height: "75%",
     position: "absolute",
     left: "5%",
     right: "5%",
